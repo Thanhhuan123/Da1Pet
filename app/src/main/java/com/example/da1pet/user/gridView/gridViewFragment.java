5a910f2e2594e72b756badc7977bb70049a0af2e
ThienDuc <ducntph25417@fpt.edu.vn>
2022-11-23 08:43:53 +0700

    theem avata thoong tin ca nhan

---
+++ app/src/main/java/com/example/da1pet/user/gridView/gridViewFragment.java
@@ -0,0 +1,33 @@
+package com.example.da1pet.user.gridView;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.da1pet.R;
+public class gridViewFragment extends Fragment {
+    public gridViewFragment() {
+        // Required empty public constructor
+    }
+
+    public static gridViewFragment newInstance() {
+        gridViewFragment fragment = new gridViewFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_grid_view, container, false);
+    }
+}
